dd878482eb20fd72689b1bd02809be3eabef6832
Juan Pablo <41731415+Juanpgm@users.noreply.github.com>
2026-03-02 11:21:51 -0500

    Mejoras en políticias de gobernanza de datos

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,5 @@
 import { expect } from 'vitest'
-import matchers from '@testing-library/jest-dom/matchers'
+import * as matchers from '@testing-library/jest-dom/matchers'
 
 // Register jest-dom matchers with Vitest's expect
 expect.extend(matchers)
